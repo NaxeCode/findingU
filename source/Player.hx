9badd000d8b147b12af7a41b3905e3c6bebe9f15
Naxe <lucas_naruto_ae@yahoo.com>
2018-10-31 00:40:42 +0200

    Initial commit

---
+++ source/Player.hx
@@ -0,0 +1,19 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+
+class Player extends FlxSprite
+{
+    public function new()
+    {
+        super();
+
+        makeGraphic(32, 32);
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elasped);
+    }
+}
