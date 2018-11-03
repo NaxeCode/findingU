5daf6fc9e1cdcea4ab5bec87523d9682950b7239
Naxe <lucas_naruto_ae@yahoo.com>
2018-11-03 18:20:38 +0200

    Implemented testChar sprite

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,22 +1,34 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.FlxSprite;
 
 class PlayState extends FlxState
 {
 	var player:Player;
+	var testChar:FlxSprite;
 
 	override public function create():Void
 	{
 		player = new Player();
 		player.screenCenter();
 		add(player);
+
+		testChar = new FlxSprite(300, 300);
+		testChar.makeGraphic(64, 64, 0xFFFF0000);
+		add(testChar);
 		
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
+		FlxG.overlap(player, testChar, function(d1, d2) {
+			if (FlxG.keys.justPressed.SPACE)
+				trace("Yo whassup");
+		});
+
 		super.update(elapsed);
 	}
 }
